0027c71a1e2f08d5ce923a958e7972ac859f26c3
sibani <bariksibani16@gmail.com>
2024-05-26 09:10:10 +0530

    Added new form for login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,16 @@
 function LoginPage(){
     return(
         <div>
-            Hello this is login page
-        </div>
+        <form action="">
+            <lebel for="username"><b>Username :</b></lebel>
+            <input type="text" name="username" /><br/>
+    
+            <lebel for="Password"><b>Password :</b></lebel>
+            <input type="password" name="Password" /><br />
+    
+            <button type="submit">Log in</button>
+        </form>
+       </div>
     )
 }
 export default LoginPage;
